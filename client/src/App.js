5c4cc45cf154576da0f282d1670faed35c71d542
RomanMereneanu <asasin84@mail.ru>
2019-10-31 22:00:56 +0200

    front

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import Post from './components/Post';
+
+import posts from './post';
+
+
 
 function App () {
+
   return (
-      <h1>Hello world</h1>
+      <div>
+          {
+              posts.map((post, key) => {
+                  return(
+                      <Post
+                          key={key}
+                          title={post.title}
+                          description={post.description}
+                          image={post.image}
+                      />
+                  )
+              })
+          }
+      </div>
   );
 }
 
---
+++ client/src/components/Post.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Post = (props) => {
+        return (
+            <div className="post">
+                <div
+                    className="post__image"
+                    style={{ backgroundImage: `url(${ props.image })` }}
+                >
+
+                </div>
+
+                <div className="post__info">
+                    <h2 className="post__title">
+                        { props.title }
+                    </h2>
+
+                    <p className="post__description">
+                        { props.description }
+                    </p>
+                </div>
+
+            </div>
+        );
+
+};
+
+export default Post;
